64baefeb31b34748f83ffd2a415b596ec40ec7c3
rodolfo007kn2 <123rod123@gmail.com>
2023-05-23 19:12:44 -0500

    esta es una prueba

---
+++ practicandoGitExamen/src/main/java/com/cibertec/grupo6/practicandoGitExamen/PracticandoGitExamenApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.grupo6.practicandoGitExamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticandoGitExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticandoGitExamenApplication.class, args);
+	}
+
+}
---
+++ practicandoGitExamen/src/test/java/com/cibertec/grupo6/practicandoGitExamen/PracticandoGitExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.grupo6.practicandoGitExamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticandoGitExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
